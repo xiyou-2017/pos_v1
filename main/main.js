5e2ff69a1416ab84d0a9aec5b1317834ece41fd8
KingintheXUPT <9786499732@qq.com>
2017-07-25 10:38:17 +0800

    posv1

--- main/main.js
+++ main/main.js
@@ -1,13 +1,7 @@
-'use strict';
+let printReceipt = function (inputs) {
+  let carItems = buildItems(inputs);
 
-let printReceipt = (inputs) => {
-  console.log(1);
-  let cartItems = buildItems(inputs);
-  let itemsSubtotal = buildReceiptItems(cartItems);
-  let receipt = buildReceipt(itemsSubtotal);
-  printCartItemsReceipt(receipt);
-
-};
+}
 
 function buildItems(inputs) {
   let allItems = loadAllItems();
@@ -34,45 +28,3 @@ function buildItems(inputs) {
   return carItems;
 }
 
-function buildReceiptItems(cartItems) {
-  let itemsSubtotal = [];
-  cartItems.forEach(function (cartItem) {
-    let Promotion = loadPromotions();
-    if (Promotion[0].barcodes.find((barcode) => {return barcode == cartItem.item.barcode})) {
-      if (Promotion[0].type == "BUY_TWO_GET_ONE_FREE") {
-        let money = cartItem.count * cartItem.item.price;
-        let saved = parseInt(cartItem.count / 3) * cartItem.item.price;
-        let subtotal = money - saved;
-        itemsSubtotal.push({cartItem, saved, subtotal});
-      }
-    }
-    else {
-      let subtotal = cartItem.count * cartItem.item.price;
-      let saved = 0;
-      itemsSubtotal.push({cartItem, saved, subtotal});
-    }
-  });
-
-  return itemsSubtotal;
-}
-
-function buildReceipt(itemsSubtotal) {
-  let savedTotal = 0;
-  let total = 0;
-
-  itemsSubtotal.forEach(function (itemsSub) {
-    savedTotal += itemsSub.saved;
-    total += itemsSub.subtotal;
-  });
-
-  return {itemsSubtotal, savedTotal, total};
-}
-
-function printCartItemsReceipt(receipt) {
-  let String = "***<没钱赚商店>收据***";
-  receipt.itemsSubtotal.forEach(function (itemsSubtotal) {
-    String += '\n名称：' + itemsSubtotal.cartItem.item.name + '，数量：' + itemsSubtotal.cartItem.count + itemsSubtotal.cartItem.item.unit + '，单价：' + itemsSubtotal.cartItem.item.price.toFixed(2) + '(元)，小计：' + itemsSubtotal.subtotal.toFixed(2) + '(元)';
-  });
-  String += '\n----------------------' + '\n总计：' + receipt.total.toFixed(2) + '(元)' + '\n节省：' + receipt.savedTotal.toFixed(2) + '(元)' + '\n**********************';
-  console.log(String);
-}
